1a69549f1986be8a76207356569a95fc9cd510cb
Adham mohammed <159294391+adham-labeb@users.noreply.github.com>
2026-02-21 17:18:04 +0200

    Update database creation and schema.sql

--- database creation and schema.sql
+++ database creation and schema.sql
@@ -6,11 +6,6 @@ Script Purpose:
     This script creates a new database named 'MyDataWareHouseTrainningProject' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'Bronze', 'Silver', and 'Gold'.
-	
-WARNING:
-    Running this script will drop the entire 'MyDataWareHouseTrainningProject' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
 */
 -- Drop and recreate the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
@@ -32,4 +27,5 @@ create schema Bronze;
 Go
 create schema Silver;
 GO
+
 create schema Gold;
